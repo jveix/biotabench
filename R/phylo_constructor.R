cb0a923bd7b33b0f8272c9f69f505dd8192834be
jveix <jveix@protonmail.com>
2025-06-18 20:41:51 -0600

    Added quality control features to phylo_constructor.

--- R/phylo_constructor.R
+++ R/phylo_constructor.R
@@ -5,13 +5,14 @@
 #' @param microbiota_data (Required). A data frame containing samples in rows and features with full taxonomy in cols.
 #' @param id_col (Required). String for a column in the data frame to be used as sample id.
 #' @param metadata (Optional). A data frame containing metadata for the experiment. Should contain the id_col.
+#' @param prev_cutoff (Optional). Prevalence cutoff for ASVs in the samples (default 5%).
 #'
 #' @returns A \link[phyloseq]{phyloseq-class} object.
 #' @export
 #'
 #' @examples
 #' phylo_constructor(test_data, id_col = "Infants_pdp_ID")
-phylo_constructor <- function(microbiota_data, id_col, metadata) {
+phylo_constructor <- function(microbiota_data, id_col, metadata, prev_cutoff = 5) {
 
   if (missing(microbiota_data)) {
     stop("No microbiota_data was provided")
@@ -34,6 +35,18 @@ phylo_constructor <- function(microbiota_data, id_col, metadata) {
   tax_names <- paste("ASV", 1:nrow(tax_table), sep = "_")
   rownames(tax_table) <- tax_names
   colnames(ft_table) <- tax_names
+
+  keep_taxa <- colSums(ft_table) > 0
+
+  if(sum(keep_taxa) < ncol(ft_table))
+    message("Dropping ", ncol(ft_table) - sum(keep_taxa), " ASVs with zero counts in all samples.")
+  ft_table <- ft_table[, keep_taxa]
+
+  taxa_prev <- ((colSums(ft_table > 0) / nrow(ft_table)) * 100)
+
+  if(any(taxa_prev < prev_cutoff))
+    message("Dropping ", sum(taxa_prev < prev_cutoff), " ASVs bellow ", prev_cutoff, "% prevalence cutoff.")
+  ft_table <- ft_table[, taxa_prev > prev_cutoff]
 
   if (!missing(metadata)) {
     return(phyloseq::phyloseq(feature_table(ft_table), phyloseq::tax_table(tax_table), phyloseq::sample_data(metadata)))
